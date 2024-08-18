5881c916eb45095d2cf887a50aad72b22650c0f8
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2024-08-18 18:22:10 -0400

    add: .env

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('init')
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+
+require('dotenv').config({path: './.env'})
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server up http://localhost:${PORT}`)
+})
