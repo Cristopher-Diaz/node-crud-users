7b2431f21203e2268e2a49c71ff2c0b99131fc79
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2024-09-01 00:05:56 -0400

    add: express ejs layouts

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const expressLayouts = require('express-ejs-layouts')
 
 // Variables desde el .env
 require('dotenv').config({path: './.env'})
@@ -7,7 +9,9 @@ const PORT = process.env.PORT || 3000
 
 // Declaramos ejs como motor de plantillas
 app.set('view engine', 'ejs')
+app.use(expressLayouts)
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/router'))
 
 
